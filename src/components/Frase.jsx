295c4d6123be3427347b3fb9ecb4af777e240d23
rebecaverasa <rebecaverasa@gmail.com>
2024-04-30 11:03:57 -0300

    Aula_04

---
+++ src/components/Frase.jsx
@@ -0,0 +1,12 @@
+function Frase() {
+
+    return(
+        <div>
+            <h1>
+                Frase é uma frase muito frase.
+            </h1>
+        </div>
+    )
+}
+
+export default Frase
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,16 @@
+import Frase from "./Frase"
+
+function HelloWorld() {
+
+    return (
+        <div>
+            <Frase />
+            <h1>Hello World!</h1>
+            <Frase />
+            <Frase />
+            <Frase />
+            <Frase />
+        </div>
+    )
+}
+export default HelloWorld
